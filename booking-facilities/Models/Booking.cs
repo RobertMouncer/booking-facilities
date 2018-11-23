940aa40d11cb75e65ad99536b630f265f29d6b01
Robert Mouncer <robertmouncer@gmail.com>
2018-11-23 21:00:34 +0000

    Data annotations
    Data annotations added to model.

--- booking-facilities/Models/Booking.cs
+++ booking-facilities/Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace booking_facilities.Models
         public virtual int FacilityId { get; set; }
         public virtual Facility Facility { get; set; }
         [Required]
+        [DisplayName("Booking Date")]
         public virtual DateTime BookingDateTime { get; set; }
         [Required]
         public virtual int UserId { get; set; }
--- booking-facilities/Models/Venue.cs
+++ booking-facilities/Models/Venue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace booking_facilities.Models
         [Required]
         public virtual int VenueId { get; set; }
         [Required]
+        [MaxLength(50)]
+        [DisplayName("Venue")]
         public virtual String VenueName { get; set; }
     }
 }
