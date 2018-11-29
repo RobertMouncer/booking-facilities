c6de72f718fbc8e5f87c72d48d66d13f99622084
ckl2 <charles.lathbury@gmail.com>
2018-11-29 16:36:46 +0000

    Communications with Gatekeeper API
    Configued communications with Gatekeeper API and included user info in booking details page

--- booking-facilities/Models/Booking.cs
+++ booking-facilities/Models/Booking.cs
@@ -18,6 +18,7 @@ namespace booking_facilities.Models
         [Required]
         [DisplayName("Booking Date & Time")]
         public virtual DateTime BookingDateTime { get; set; }
+        [DisplayName("User Email")]
         public virtual String UserId { get; set; }
     }
 }
