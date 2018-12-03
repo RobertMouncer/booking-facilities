0719e9d01265c9521c2390be27cf1ec5ca74d2d5
Robert Mouncer <robertmouncer@gmail.com>
2018-12-03 15:39:23 +0000

    Generators

---
+++ booking-facilitiesTest/TestUtils/SportGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using booking_facilities.Models;
+
+namespace booking_facilitiesTest.TestUtils
+{
+    class SportGenerator
+    {
+        public static Sport Create(int index = 0)
+        {
+            return new Sport
+            {
+                SportId = index,
+                SportName = "Cricket" + index
+            };
+        }
+
+        public static List<Sport> CreateList(int length = 5)
+        {
+            List<Sport> list = new List<Sport>();
+            for (var i = 0; i < length; i++)
+            {
+                list.Add(Create(i));
+            }
+            return list;
+        }
+    }
+}
---
+++ booking-facilitiesTest/TestUtils/VenueGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using booking_facilities.Models;
+
+namespace booking_facilitiesTest.TestUtils
+{
+    class VenueGenerator
+    {
+        public static Venue Create(int index = 0)
+        {
+            return new Venue
+            {
+                VenueId = index,
+                VenueName = "Old Sports Hall" + index
+            };
+        }
+
+        public static List<Venue> CreateList(int length = 5)
+        {
+            List<Venue> list = new List<Venue>();
+            for (var i = 0; i < length; i++)
+            {
+                list.Add(Create(i));
+            }
+            return list;
+        }
+    }
+}
