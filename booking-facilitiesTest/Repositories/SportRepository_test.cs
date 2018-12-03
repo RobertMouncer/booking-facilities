b347f8f76d838d2d604b4be31fa6598fe2d10bed
Robert Mouncer <robertmouncer@gmail.com>
2018-12-03 18:07:35 +0000

    Begun writing tests

---
+++ booking-facilitiesTest/Repositories/SportRepository_test.cs
@@ -0,0 +1,31 @@
+﻿using booking_facilities.Models;
+using booking_facilitiesTest.TestUtils;
+using booking_facilities.Repositories;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace booking_facilitiesTest.Repositories
+{
+    public class SportRepository_Test
+    {
+        private static readonly Random random = new Random();
+        private readonly DbContextOptions<booking_facilitiesContext> contextOptions;
+
+        public SportRepository_Test()
+        {
+            contextOptions = new DbContextOptionsBuilder<booking_facilitiesContext>()
+                .UseInMemoryDatabase($"rand_db_name_{random.Next()}").Options;
+        }
+
+        [Fact]
+        public async void AddAsync_AddAsyncToConext()
+        {
+
+        }
+
+    }
+}
